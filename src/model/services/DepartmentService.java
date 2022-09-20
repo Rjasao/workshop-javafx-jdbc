6b96431bfa05dcdf149e09dd1644a1033ceab4a4
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-20 03:31:53 -0300

    DepartmentForm (dialog) design
    Janela flutuante insere departamento

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -11,15 +11,7 @@ public class DepartmentService {
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 	
 	public List<Department> findAll(){
-		// dados MOCKados
-		//List<Department> list = new ArrayList<>();
-		//list.add(new Department(1, "Book"));
-		//list.add(new Department(2, "Computers"));
-		//list.add(new Department(3, "Eletronics"));
-		//return list;
 		return dao.findAll();
-		
 	}
-
 }
 
