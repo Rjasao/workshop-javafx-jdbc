ade79aad49777523ad4e1f9adde366b651fbc83e
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-19 22:04:04 -0300

    Adding database access MySQL
    Acessando MySQL... não consegui com WSL windows

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,19 +1,25 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+	
 	public List<Department> findAll(){
 		// dados MOCKados
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Book"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		return list;
+		//List<Department> list = new ArrayList<>();
+		//list.add(new Department(1, "Book"));
+		//list.add(new Department(2, "Computers"));
+		//list.add(new Department(3, "Eletronics"));
+		//return list;
+		return dao.findAll();
+		
 	}
 
 }
+
